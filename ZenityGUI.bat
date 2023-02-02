b3f4645f20f318d7d8398980e0e4cee4c427ff83
justaCasualCoder <120528794+justaCasualCoder@users.noreply.github.com>
2023-02-02 14:21:18 -0500

    Adding Heimdall

--- ZenityGUI.bat
+++ ZenityGUI.bat
@@ -1,4 +1,5 @@
 @echo off
+set PART=
 if exist zenity.exe (
   echo "Zenity was found!"
 ) else (
@@ -9,5 +10,5 @@ if exist zenity.exe (
 echo "Removing Temporary Directory..."
 zenity --info --title="Plug in your Samsung device" --text=" To enter download mode, Power off your device and then press and hold volume-down and power button. Then when the Download mode screen appears , press volume up to confirm entering Download mode. ( This may be differant on newer models - try typing it in to Google)"
 rd /s /q "temp"
-FOR /F %%g IN ('zenity --list --title="Samsung Download Flasher" --text "What Partition do you want to flash?" "Boot" "Recovery" "Data" "System"') do (SET VAR=%%g && if not %ERRORLEVEL%==0 ( echo "You chose to quit!"))
-echo %VAR%
+FOR /F %%g IN ('zenity --list --title="Samsung Download Flasher" --text "What Partition do you want to flash?" "Boot" "Recovery" "Data" "System"') do (SET PART=%%g && if not %ERRORLEVEL%==0 then echo "You chose to quit!")
+if "%PART%"=="" (echo Quiting... && exit 0)
