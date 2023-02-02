35784e42d43a06f6fe44ed0dfe7a5be47f00cb41
justaCasualCoder <120528794+justaCasualCoder@users.noreply.github.com>
2023-02-02 13:43:52 -0500

    Update ZenityGUI.bat

--- ZenityGUI.bat
+++ ZenityGUI.bat
@@ -9,14 +9,5 @@ if exist zenity.exe (
 echo "Removing Temporary Directory..."
 zenity --info --title="Plug in your Samsung device" --text=" To enter download mode, Power off your device and then press and hold volume-down and power button. Then when the Download mode screen appears , press volume up to confirm entering Download mode. ( This may be differant on newer models - try typing it in to Google)"
 rd /s /q "temp"
-FOR /F %g IN ('zenity --list \
---title="Samsung Download Flasher" \
---text "What Partition do you want to flash?" \
---radiolist \
---column "Pick" \
---column "Partition" \
-FALSE "Boot" \
-FALSE "Recovery" \
-FALSE "Data" \
-FALSE "System"') do (SET VAR=%PART)
-
+FOR /F %%g IN ('zenity --list --title="Samsung Download Flasher" --text "What Partition do you want to flash?" "Boot" "Recovery" "Data" "System"') do (SET VAR=%%g && if not %ERRORLEVEL%==0 ( echo "You chose to quit!"))
+echo %VAR%
