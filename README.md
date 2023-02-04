afee8aef764fec54761c56a6cd7445edaee56b89
justaCasualCoder <120528794+justaCasualCoder@users.noreply.github.com>
2023-02-04 01:08:38 -0500

    Summary

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # SamsungFlashGUI
 A Script that can be used to easily flash .img files to any samsung phone using Linux (Windows is being developed)
-## Screenshots
+##### Screenshots
 <img src="Images/FileSelect.png" width="600px" > <img src="Images/SelectPart.png" width="600px" >
+## Instructions
+#### Windows
+Download the Windows.zip from the releases section and extarct it. Then enter the folder that you extarcted the zip to and run the zadig tool inside the heimdall directory. Then go to the top of of the program and press Options/List all devices. After that go to the device slection area and select your device. It should be something like MSM****. Then cycle through the driver,s and replace each driver. 
+After You have done this, go to the parent folder ( the directory that you unzipped to) and run flashgui.bat
+  
--- ZenityGUI.bat
+++ ZenityGUI.bat
@@ -9,7 +9,7 @@ if exist zenity.exe (
   exit
 )
 zenity --info --title="Plug in your Samsung device" --text=" To enter download mode, Power off your device and then press and hold volume-down and power button. Then when the Download mode screen appears , press volume up to confirm entering Download mode. ( This may be differant on newer models - try typing it in to Google)"
-FOR /F %%g IN ('zenity --list --title="Samsung Download Flasher" --text "What Partition do you want to flash?" "Boot" "Recovery" "Data" "System"') do (SET PART=%%g )
+FOR /F %%g IN ('zenity --list --no-cancel --title="Samsung Download Flasher" --text "What Partition do you want to flash?" "Boot" "Recovery" "Data" "System"') do (SET PART=%%g )
 if "%PART%"=="" do (echo Quiting... && exit 0 )
 :select_file
 FOR /F %%g IN ('zenity --file-selection --title="Please select the *.IMG file to be flashed" --file-filter=*img') do (SET FILE=%%g && echo %FILE%)
@@ -20,19 +20,17 @@ echo Quiting... && exit 0
 mkdir temp
 if "%PART%"=="Data" (
 echo "Please see terminal output for progress"
-powershell "heimdall/heimdall.exe flash --RECOVERY %FILE% 2>&1 | tee output.txt"
+heimdall/heimdall.exe flash --RECOVERY %FILE%
 )
 if "%PART%"=="Recovery" (
 echo "Please see terminal output for progress"
-powershell "heimdall/heimdall.exe flash --RECOVERY %FILE% 2>&1 | tee output.txt"
+heimdall/heimdall.exe flash --RECOVERY %FILE%
 )
 if "%PART%"=="Boot" (
 echo "Please see terminal output for progress"
-powershell "heimdall/heimdall.exe flash --RECOVERY %FILE% 2>&1  | tee "output.txt"
+heimdall/heimdall.exe flash --RECOVERY %FILE%
 )
 if "%PART%"=="System" do (
 echo "Please see terminal output for progress"
-powershell "heimdall/heimdall.exe flash --RECOVERY %FILE% 2>&1 | tee "output.txt"
+heimdall/heimdall.exe flash --RECOVERY %FILE%
 )
-FindStr /IC:"ERROR: Failed to detect compatible download-mode device" "output.txt" || (zenity --error --text="No Compatible Device plugged in download mode - Restarting..." && goto top)
-del output.txt
